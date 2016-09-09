2004959bd874ff738a2bd92418bf9c357cc695be
tomhon <tomhon@hotmail.com>
2016-09-08 21:04:07 -0700

    added bot state machine sample

--- app.js
+++ app.js
@@ -23,9 +23,49 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello Parrot World");
-});
+// bot.dialog('/', function (session) {
+//     session.send("Hello Parrot World");
+// });
+
+bot.dialog('/', [
+    function (session) {
+        session.send("You're in a large clearing. There's a path to the north.");
+        builder.Prompts.choice(session, "command?", ["north", "look"]);
+    },
+    function (session, results) {
+        switch (results.repsonse.entity) {
+            case "north":
+                session.replaceDialog("/room1");
+                break;
+            default:
+                session.replaceDialog("/");
+                break;
+        }
+    }
+]);
+bot.dialog('/room1', [
+    function (session) {
+        session.send("There's a small house here surrounded by a white fence with a gate. There's a path to the south and west.");
+        builder.Prompts.choice(session, "command?", ["open gate", "south", "west", "look"]);
+    },
+    function (session, results) {
+        switch (results.repsonse.entity) {
+            case "open gate":
+                session.replaceDialog("/room2");
+                break;
+            case "south":
+                session.replaceDialog("/");
+                break;
+            case "west":
+                session.replaceDialog("/room3");
+                break;
+            default:
+                session.replaceDialog("/room1");
+                break;
+        }
+    }
+]);
+
 
 server.get('/', function (req, res) { 
     res.send('Parrot Bot is online'); 
