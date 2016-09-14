2abd09c5fbb2b08c98a0d30a2b822bf30ca00978
Tom Honeybone <Tom Honeybone>
2016-09-13 22:54:06 -0700

    added button

--- app.js
+++ app.js
@@ -81,6 +81,7 @@ bot.dialog('/cards', [
                 new builder.HeroCard(session)
                     .title("Hero Card")
                     .subtitle("Space Needle")
+                    .buttons("Press Me")
                     .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
                     .images([
                         builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
