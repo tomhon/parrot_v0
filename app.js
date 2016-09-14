732706db1aa0e308f1007434b837e27474d8ee09
Tom Honeybone <Tom Honeybone>
2016-09-13 22:49:28 -0700

    added card

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ bot.dialog('/gameDetails', [
     function (session, results) {
         switch (results.response.entity) {
             case "Teams":
-                session.replaceDialog("/teams");
+                session.replaceDialog("/cards");
                 break;
             case "Location":
                 session.replaceDialog("/location");
@@ -73,7 +73,23 @@ bot.dialog('/gameDetails', [
     }
 ]);
 
-
+bot.dialog('/cards', [
+    function (session) {
+        var msg = new builder.Message(session)
+            .textFormat(builder.TextFormat.xml)
+            .attachments([
+                new builder.HeroCard(session)
+                    .title("Hero Card")
+                    .subtitle("Space Needle")
+                    .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
+                    .images([
+                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
+                    ])
+                    .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
+            ]);
+        session.endDialog(msg);
+    }
+]);
 server.get('/', function (req, res) { 
     res.send('Parrot Bot is online'); 
     }); 
