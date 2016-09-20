9161b40e94177473b5e01b6165454fc24001d90a
Tom Honeybone <tomhon@hotmail.com>
2016-09-19 22:41:29 -0700

    1 button

--- app.js
+++ app.js
@@ -166,8 +166,7 @@ bot.dialog('/actions', [
                         builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
                     ])
                     .buttons([
-                        builder.CardAction.dialogAction(session, "weather", "Seattle, WA", "Current SEA Weather")
-                        // ,builder.CardAction.dialogAction(session, "weather", "Portland, OR", "Current PDC Weather")
+                        builder.CardAction.dialogAction(session, "weather", "Seattle, WA", "Current Weather")
                     ])
             ]);
         session.send(msg);
