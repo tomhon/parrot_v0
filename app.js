deebada127de6d902b09a3069333a1320478dcee
Tom Honeybone <tomhon@hotmail.com>
2016-09-19 22:34:03 -0700

    1 button

--- app.js
+++ app.js
@@ -166,8 +166,8 @@ bot.dialog('/actions', [
                         builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
                     ])
                     .buttons([
-                        builder.CardAction.dialogAction(session, "SEA weather", "Seattle, WA", "Current Weather"),
-                        builder.CardAction.dialogAction(session, "PDC weather", "Portland, OR", "Current Weather")
+                        builder.CardAction.dialogAction(session, "SEA weather", "Seattle, WA", "Current Weather")
+                        // ,builder.CardAction.dialogAction(session, "PDC weather", "Portland, OR", "Current Weather")
                     ])
             ]);
         session.send(msg);
