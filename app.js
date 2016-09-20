cac51ddf1729f29f6ea4e64346566726457e1340
tomhon <tomhon@hotmail.com>
2016-09-20 12:20:48 -0700

    updated first menu

--- app.js
+++ app.js
@@ -55,158 +55,79 @@ bot.dialog('/', [
     }
 ]);
 
-bot.dialog('/menuCarousel', [
+bot.dialog('/menu', [
     function (session) {
         session.send("You can pass a custom message to Prompts.choice() that will present the user with a carousel of cards to select from. Each card can even support multiple actions.");
         
         // Ask the user to select an item from a carousel.
-            var msg = new builder.Message(session)
+        var msg = new builder.Message(session)
             .textFormat(builder.TextFormat.xml)
             .attachmentLayout(builder.AttachmentLayout.carousel)
             .attachments([
-                new builder.HeroCard(session)
-                    .title("Space Needle")
-                    .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
-                    .images([
-                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
-                            .tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/800px-Seattlenighttimequeenanne.jpg")),
-                    ])
+                 new builder.HeroCard(session)
+                    .title("Welcome to Parrot")
+                    // .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
+                    // .images([
+                    //     builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
+                    //         .tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/800px-Seattlenighttimequeenanne.jpg")),
+                    // ])
                     .buttons([
-                        builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle", "Wikipedia"),
-                        builder.CardAction.imBack(session, "select:100", "Select")
+                        builder.CardAction.imBack(session, "select:100", "Login"),
+                        builder.CardAction.imBack(session, "select:100", "Match Details")
                     ]),
-                new builder.HeroCard(session)
-                    .title("Pikes Place Market")
-                    .text("<b>Pike Place Market</b> is a public market overlooking the Elliott Bay waterfront in Seattle, Washington, United States.")
-                    .images([
-                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/320px-PikePlaceMarket.jpg")
-                            .tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/800px-PikePlaceMarket.jpg")),
-                    ])
-                    .buttons([
-                        builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Pike_Place_Market", "Wikipedia"),
-                        builder.CardAction.imBack(session, "select:101", "Select")
-                    ]),
-                new builder.HeroCard(session)
-                    .title("EMP Museum")
-                    .text("<b>EMP Musem</b> is a leading-edge nonprofit museum, dedicated to the ideas and risk-taking that fuel contemporary popular culture.")
-                    .images([
-                        builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Night_Exterior_EMP.jpg/320px-Night_Exterior_EMP.jpg")
-                            .tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Night_Exterior_EMP.jpg/800px-Night_Exterior_EMP.jpg"))
-                    ])
-                    .buttons([
-                        builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/EMP_Museum", "Wikipedia"),
-                        builder.CardAction.imBack(session, "select:102", "Select")
-                    ])
+     
             ]);
-        // var msg = new builder.Message(session)
-        //     .textFormat(builder.TextFormat.xml)
-        //     .attachmentLayout(builder.AttachmentLayout.carousel)
-        //     .attachments([
-        //         new builder.HeroCard(session)
-        //             .title("What's happened so far?")
-        //             .text("I can tell you the latst score or give you a full run down of the highlights so far.")
-        //             .images([
-        //                 builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
-        //                     .tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/800px-Seattlenighttimequeenanne.jpg")),
-        //             ])
-        //             .buttons([
-        //                 builder.CardAction.imBack(session, "select:100", "Latest Score?"),
-        //                 builder.CardAction.imBack(session, "select:100", "What's happened?")
-        //             ]),
-        //         new builder.HeroCard(session)
-        //             .title("Did you see that?")
-        //             .text("Did you hear a whistle? Did someone score? Did someone take a shot? Did the ref blow the whistle?")
-        //             .images([
-        //                 builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/320px-PikePlaceMarket.jpg")
-        //                     .tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/PikePlaceMarket.jpg/800px-PikePlaceMarket.jpg")),
-        //             ])
-        //             .buttons([
-        //                 builder.CardAction.imBack(session, "select:101", "Whistle"),
-        //                 builder.CardAction.imBack(session, "select:101", "Goal"),
-        //                 builder.CardAction.imBack(session, "select:101", "Shot")
-        //             ]),
-        //         new builder.HeroCard(session)
-        //             .title("Match Details")
-        //             .text("Let me know details of the match. Who's Playing? What's the location? What time does it start? What's the weather?")
-        //             .images([
-        //                 builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Night_Exterior_EMP.jpg/320px-Night_Exterior_EMP.jpg")
-        //                     .tap(builder.CardAction.showImage(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Night_Exterior_EMP.jpg/800px-Night_Exterior_EMP.jpg"))
-        //             ])
-        //             .buttons([
-        //                 builder.CardAction.imBack(session, "select:102", "Teams"),
-        //                 builder.CardAction.imBack(session, "select:102", "Location"),
-        //                 builder.CardAction.imBack(session, "select:102", "Schedule"),
-        //                 builder.CardAction.imBack(session, "select:102", "Weather")
-
-        //             ])
-        //     ]);
         builder.Prompts.choice(session, msg, "select:100|select:101|select:102");
     },
     function (session, results) {
-        if (results.response && results.response.entity != '(quit)') {
-            // Launch demo dialog
-            session.beginDialog('/' + results.response.entity);
-        } else {
-            // Exit the menu
-            session.endDialog();
+        var action, item;
+        var kvPair = results.response.entity.split(':');
+        switch (kvPair[0]) {
+            case 'select':
+                action = 'selected';
+                break;
         }
-    },
-    function (session, results) {
-        // The menu runs a loop until the user chooses to (quit).
-        session.replaceDialog('/menu');
-    }
-]).reloadAction('reloadMenu', null, { matches: /^menu|show menu/i });
-
-//         var action, item;
-//         var kvPair = results.response.entity.split(':');
-//         switch (kvPair[0]) {
-//             case 'select':
-//                 action = 'selected';
-//                 break;
-//         }
-//         switch (kvPair[1]) {
-//             case '100':
-//                 item = "the <b>Space Needle</b>";
-//                 break;
-//             case '101':
-//                 item = "<b>Pikes Place Market</b>";
-//                 break;
-//             case '102':
-//                 item = "the <b>EMP Museum</b>";
-//                 break;
-//         }
-//         session.endDialog('You %s "%s"', action, item);
-//     }    
-// ]);
-
-
-
-bot.dialog('/menu', [
-    function (session) {
-        session.send("Let me know what's going on in the game and I can give you a summary anytime you need it.");
-        builder.Prompts.choice(session, "What's the latest score?, What's happened so far?, It's a Goal!, Someone took a shot, Ref blew the whistle, Here are the match details", 
-        ["Latest Score", "Ticker", "Goal", "Shot", "Whistle", "Match Details", "Actions"]);
-    },
-    function (session, results) {
-        if (results.response && results.response.entity != '(quit)') {
-            // Launch demo dialog
-            session.beginDialog('/' + results.response.entity);
-        } else {
-            // Exit the menu
-            session.endDialog();
+        switch (kvPair[1]) {
+            case '100':
+                item = "the <b>Space Needle</b>";
+                break;
+            case '101':
+                item = "<b>Pikes Place Market</b>";
+                break;
+            case '102':
+                item = "the <b>EMP Museum</b>";
+                break;
         }
-    },
-    function (session, results) {
-        // The menu runs a loop until the user chooses to (quit).
-        session.replaceDialog('/menu');
-    }
-]).reloadAction('reloadMenu', null, { matches: /^menu|show menu/i });
-
-bot.dialog('/help', [
-    function (session) {
-        session.endDialog("Global commands that are available anytime:\n\n* menu - Exits current function and returns to the menu.\n* goodbye - End this conversation.\n* help - Displays these commands.");
-    }
+        session.endDialog('You %s "%s"', action, item);
+    }    
 ]);
+
+// bot.dialog('/menu', [
+//     function (session) {
+//         session.send("Let me know what's going on in the game and I can give you a summary anytime you need it.");
+//         builder.Prompts.choice(session, "What's the latest score?, What's happened so far?, It's a Goal!, Someone took a shot, Ref blew the whistle, Here are the match details", 
+//         ["Latest Score", "Ticker", "Goal", "Shot", "Whistle", "Match Details", "Actions"]);
+//     },
+//     function (session, results) {
+//         if (results.response && results.response.entity != '(quit)') {
+//             // Launch demo dialog
+//             session.beginDialog('/' + results.response.entity);
+//         } else {
+//             // Exit the menu
+//             session.endDialog();
+//         }
+//     },
+//     function (session, results) {
+//         // The menu runs a loop until the user chooses to (quit).
+//         session.replaceDialog('/menu');
+//     }
+// ]).reloadAction('reloadMenu', null, { matches: /^menu|show menu/i });
+
+// bot.dialog('/help', [
+//     function (session) {
+//         session.endDialog("Global commands that are available anytime:\n\n* menu - Exits current function and returns to the menu.\n* goodbye - End this conversation.\n* help - Displays these commands.");
+//     }
+// ]);
 
 
 bot.dialog('/Match Details', [
