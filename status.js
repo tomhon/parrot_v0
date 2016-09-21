2a6e7f00b654a2b5d566fac9110c4fd9872c60e0
tomhon <tomhon@hotmail.com>
2016-09-21 11:57:41 -0700

    taken out functionality from status.js

---
+++ status.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+// router.get('/status', function (req, res) { 
+
+//     res.send('Parrot Bot Status is online'); 
+//     }); 
+
+// module.exports = router;
