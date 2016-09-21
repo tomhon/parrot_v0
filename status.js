a109bf9bfac95f1cb72bbcf7d7e35c1665b687c8
tomhon <tomhon@hotmail.com>
2016-09-21 13:09:45 -0700

    update status.js

--- status.js
+++ status.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-
+router.get('/', function (req, res) {
+    // res.render('status' , {
+    //     mappingList: cache.get("TEBEMappingList", arrayIsvTE)
+    // });
+    res.send('Parrot Bot Status is online'); 
+    // res.send('Status Home Page')
+    });
 
 // router.get('/status', function (req, res) { 
 
 //     res.send('Parrot Bot Status is online'); 
 //     }); 
 
-// module.exports = router;
+module.exports = router;
